376be00f6a6245948282d6c03037b824c9381dfc
Taras <tbatyuchok@gmail.com>
2020-11-06 16:12:08 +0200

    add burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header'),
-//   menuItem = document.querySelectorAll('.menu_item'),
   hamburger = document.querySelector('.hamburger');
 
   hamburger.addEventListener('click', () => {
@@ -8,10 +7,6 @@ window.addEventListener('DOMContentLoaded', () => {
       hamburger.classList.toggle('hamburger_active');
   });
 
-//   menuItem.forEach(item => {
-//       item.addEventListener('click', () => {
-//           hamburger.classList.toggle('hamburger_active');
-//           menu.classList.toggle('menu_active');
-//       })
-//   })
-})
+});
+
+
