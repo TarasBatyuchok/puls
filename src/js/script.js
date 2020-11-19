e23f7f30824ca7cc8f71d7860ebc5f8d5e17f56a
Taras <tbatyuchok@gmail.com>
2020-11-19 17:00:19 +0200

    validate

--- src/js/script.js
+++ src/js/script.js
@@ -144,12 +144,13 @@ $(document).ready(function () {
         }
     });
 
+    new WOW().init();
 
 });
 
 // smooth scroll
 $(function(){
-    $("a[href^='#']").click(function(){
+    $("a[href=#up]").click(function(){
             var _href = $(this).attr("href");
             $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
             return false;
