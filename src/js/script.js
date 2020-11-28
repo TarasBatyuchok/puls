2dc7fab8957eebbd0e0a762f93aeae3f7e709fb5
Taras <tbatyuchok@gmail.com>
2020-11-28 10:34:02 +0200

    media

--- src/js/script.js
+++ src/js/script.js
@@ -14,8 +14,8 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
@@ -23,8 +23,8 @@ $(document).ready(function () {
                 breakpoint: 992,
                 settings: {
                     arrows: false,
-                    autoplay: true,
-                    autoplaySpeed: 2000,
+                    // autoplay: true,
+                    // autoplaySpeed: 2000,
                     centerPadding: '40px',
                 }
             }
